999827e695e0e0ef409fa130cddab0178ec5474a
takashiro018 <shiro.takahashi@jade.finance>
2024-02-16 15:35:58 +1000

    commiting files

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -216,3 +216,36 @@ export function InvoicesTableSkeleton() {
     </div>
   );
 }
+
+export function CustomersTableSkeleton() {
+  return (
+    <table className="hidden min-w-full text-gray-900 md:table">
+      <thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
+        <tr>
+          <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+            Name
+          </th>
+          <th scope="col" className="px-3 py-5 font-medium">
+            Email
+          </th>
+          <th scope="col" className="px-3 py-5 font-medium">
+            Total Invoices
+          </th>
+          <th scope="col" className="px-3 py-5 font-medium">
+            Total Pending
+          </th>
+          <th scope="col" className="px-4 py-5 font-medium">
+            Total Paid
+          </th>
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-gray-200 text-gray-900">
+        <TableRowSkeleton />
+        <TableRowSkeleton />
+        <TableRowSkeleton />
+        <TableRowSkeleton />
+        <TableRowSkeleton />
+      </tbody>
+    </table>
+  );
+}
