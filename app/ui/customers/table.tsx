cabb928f1746476b3572704bafe64d227c39f959
takashiro018 <shiro.takahashi@jade.finance>
2024-02-16 16:12:36 +1000

    commiting files

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -11,9 +11,6 @@ export default async function CustomersTable({
 
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
