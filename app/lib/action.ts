16bbc80aa46cb56e4c4807cfce9c344eac2dc4e3
takashiro018 <shiro.takahashi@jade.finance>
2024-02-19 09:52:08 +1000

    commiting files

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -141,3 +141,119 @@ export async function deleteInvoice(id: string) {
         return { message: 'Database Error: Failed to Delete Invoice' };
     }
 }
+
+const CFormSchema = z.object({
+    name: z.string(),
+    customerName: z.string({
+        invalid_type_error: 'Please enter customer name.',
+    }),
+    email: z.string(),
+    customerEmail: z.string({
+        invalid_type_error: 'Please enter customer email.',
+    }),
+    image_url: z.string(),
+    customerImg: z.string({
+        invalid_type_error: 'Please enter customer image.',
+    }),
+});
+
+const CreateCustomer = CFormSchema.omit({ name: true, email: true, image_url: true });
+
+export type cState = {
+    errors?: {
+        customerName?: string[];
+        customerEmail?: string[];
+        customerImg?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createCustomer(prevState: cState, formData: FormData) {
+
+    // Validate form using Zod
+    const validatedFields = CreateCustomer.safeParse({
+        customerName: formData.get('name'),
+        customerEmail: formData.get('email'),
+        customerImg: formData.get('image_url'),
+    });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Customer.',
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerName, customerEmail, customerImg } = validatedFields.data;
+
+    // Insert data into the database
+    try {
+        await sql`
+        INSERT INTO customers (id, name, email, image_url)
+        VALUES (id, ${customerName}, ${customerEmail}, ${customerImg})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+    } catch (error) {
+        // If a database error occurs, return a more specific error.
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
+
+    // Revalidate the cache for the invoices page and redirect the user.
+    revalidatePath('/dashboard/customers');
+    redirect('/dashboard/customers');
+}
+
+/*const UpdateCustomer = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+        return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+
+export async function deleteInvoice(id: string) {
+    //throw new Error('Failed to Delete Invoice');
+
+    // Unreachable code block
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice' };
+    }
+}*/
---
+++ app/ui/customers/create-form.tsx
@@ -0,0 +1,124 @@
+'use client';
+
+import { CustomerField } from '@/app/lib/definitions';
+import Link from 'next/link';
+import {
+    CheckIcon,
+    ClockIcon,
+    CurrencyDollarIcon,
+    UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import { Button } from '@/app/ui/button';
+import { createCustomer } from '@/app/lib/action';
+import { useFormState } from 'react-dom';
+
+export default function Form({ customers }: { customers: CustomerField[] }) {
+    const initialState = { message: null, errors: {} };
+    const [state, dispatch] = useFormState(createCustomer, initialState);
+    console.log(state);
+
+    return (
+        <form action={dispatch}>
+            <div className="rounded-md bg-gray-50 p-4 md:p-6">
+                {/* Customer Name */}
+                <div className="mb-4">
+                    <div className="sm:col-span-3">
+                        <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                            First name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                name="first-name"
+                                id="first-name"
+                                autoComplete="given-name"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="sm:col-span-3">
+                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                            Last name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                name="last-name"
+                                id="last-name"
+                                autoComplete="family-name"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div id="customer-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.customerName &&
+                            state.errors.customerName.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
+                    </div>
+                </div>
+                {/* Customer Email */}
+                <div className="mb-4">
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                        Email address
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                    <div id="customer-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.customerEmail &&
+                            state.errors.customerEmail.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
+                    </div>
+                </div>
+
+                {/* Customer Image */}
+                <fieldset>
+                    <div className="col-span-full">
+                        <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
+                            Photo
+                        </label>
+                        <div className="mt-2 flex items-center gap-x-3">
+                            <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+                            <button
+                                type="button"
+                                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                            >
+                                Upload
+                            </button>
+                        </div>
+                    </div>
+                </fieldset>
+                <div id="customer-error" aria-live="polite" aria-atomic="true">
+                    {state.errors?.customerImg &&
+                        state.errors.customerImg.map((error: string) => (
+                            <p className="mt-2 text-sm text-red-500" key={error}>
+                                {error}
+                            </p>
+                        ))}
+                </div>
+            </div>
+            <div className="mt-6 flex justify-end gap-4">
+                <Link
+                    href="/dashboard/customers"
+                    className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+                >
+                    Cancel
+                </Link>
+                <Button type="submit">Create Customer</Button>
+            </div>
+        </form>
+    );
+}
