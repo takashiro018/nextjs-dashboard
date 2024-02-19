26bf41a77178222eda5c3209126ba60dcf44ec56
takashiro018 <shiro.takahashi@jade.finance>
2024-02-20 09:50:11 +1000

    commiting files

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,6 +1,6 @@
 import mime from "mime";
 import { join } from "path";
-import { stat, mkdir, writeFile } from "fs/promises";
+import { stat, writeFile } from "fs/promises";
 //import * as dateFn from "date-fns";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -15,26 +15,11 @@ export async function POST(request: NextRequest) {
         );
     }
 
+    console.log(formData);
+    console.log(file);
     const buffer = Buffer.from(await file.arrayBuffer());
     const relativeUploadDir = `/customers/`;
     const uploadDir = join(process.cwd(), "public", relativeUploadDir);
-
-    try {
-        await stat(uploadDir);
-    } catch (e: any) {
-        if (e.code === "ENOENT") {
-            await mkdir(uploadDir, { recursive: true });
-        } else {
-            console.error(
-                "Error while trying to create directory when uploading a file\n",
-                e
-            );
-            return NextResponse.json(
-                { error: "Something went wrong." },
-                { status: 500 }
-            );
-        }
-    }
 
     try {
         //const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
@@ -42,6 +27,7 @@ export async function POST(request: NextRequest) {
             /\.[^/.]+$/,
             ""
         )}.${mime.getExtension(file.type)}`;
+        console.log(filename);
         await writeFile(`${uploadDir}/${filename}`, buffer);
         return NextResponse.json({ fileUrl: `${relativeUploadDir}/${filename}` });
     } catch (e) {
