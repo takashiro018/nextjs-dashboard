337fd856ddfffbcf8a1b3c714345ddb6268065ed
takashiro018 <shiro.takahashi@jade.finance>
2024-02-20 09:24:48 +1000

    commiting files

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -6,6 +6,8 @@ export async function POST(request: NextRequest) {
     const data = await request.formData()
     const file: File | null = data.get('file') as unknown as File
 
+    console.log(data);
+    console.log(file);
     if (!file) {
         return NextResponse.json({ success: false })
     }
@@ -13,9 +15,13 @@ export async function POST(request: NextRequest) {
     const bytes = await file.arrayBuffer()
     const buffer = Buffer.from(bytes)
 
+    console.log(bytes);
+    console.log(buffer);
+
     // With the file data in the buffer, you can do whatever you want with it.
     // For this, we'll just write it to the filesystem in a new location
     const path = join('/', 'public/customers', file.name)
+    console.log(path);
     await writeFile(path, buffer)
     console.log(`open ${path} to see the uploaded file`)
 
