52644c31d5e9610bd33f9ccef31eb4cc1dfc7e61
takashiro018 <shiro.takahashi@jade.finance>
2024-02-20 10:40:55 +1000

    commiting files

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -7,7 +7,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
     const formData = await request.formData();
 
-    const file = formData.get('image_url');
+    const file = formData.get('image_url') as Blob | null;
     if (!file) {
         return NextResponse.json(
             { error: "File blob is required." },
@@ -17,16 +17,19 @@ export async function POST(request: NextRequest) {
 
     console.log(formData);
     console.log(file);
-    //const buffer = Buffer.from(await file.arrayBuffer());
-    //const relativeUploadDir = `/customers/`;
-    //const uploadDir = join(process.cwd(), "public", relativeUploadDir);
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const relativeUploadDir = `/customers/`;
+    const uploadDir = join(process.cwd(), "public", relativeUploadDir);
 
+    console.log(buffer);
+    console.log(relativeUploadDir);
+    console.log(uploadDir);
     /*try {
         //const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-        const filename = `${file.replace(
+        //const filename = `${file.replace(
             /\.[^/.]+$/,
             ""
-        )}.${mime.getExtension(file.type)}`;
+        //)}.${mime.getExtension(file.type)}`;
         console.log(filename);
         await writeFile(`${uploadDir}/${filename}`, buffer);
         return NextResponse.json({ fileUrl: `${relativeUploadDir}/${filename}` });
