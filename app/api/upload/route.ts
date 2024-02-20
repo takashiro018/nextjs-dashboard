f91a068ef1999770548b3873cfe05038d9cd2617
takashiro018 <shiro.takahashi@jade.finance>
2024-02-20 10:21:09 +1000

    commiting files

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,30 +1,40 @@
-import { IncomingForm } from 'formidable'
-import { promises as fs } from 'fs'
-import { IncomingMessage } from 'http';
-import { NextRequest, NextResponse } from 'next/server'
+import mime from "mime";
+import { join } from "path";
+import { stat, writeFile } from "fs/promises";
+//import * as dateFn from "date-fns";
+import { NextRequest, NextResponse } from "next/server";
 
-var mv = require('mv');
+export async function POST(request: NextRequest) {
+    const formData = await request.formData();
 
-export const config = {
-    api: {
-        bodyParser: false,
+    const file = formData.get('image_url') as Blob | null;
+    if (!file) {
+        return NextResponse.json(
+            { error: "File blob is required." },
+            { status: 400 }
+        );
     }
-};
 
-export default async (NextRequest: IncomingMessage, NextResponse: any) => {
+    console.log(formData);
+    console.log(file);
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const relativeUploadDir = `/customers/`;
+    const uploadDir = join(process.cwd(), "public", relativeUploadDir);
 
-    const data = await new Promise((resolve, reject) => {
-        const form = new IncomingForm()
-
-        form.parse(NextRequest, (err, fields, files) => {
-            if (err) return reject(err)
-            console.log(fields, files)
-            //console.log(files.file.filepath)
-            //var oldPath = files.file.filepath;
-            //var newPath = `./public/uploads/${files.file.originalFilename}`;
-            //mv(oldPath, newPath, function(err: any) {
-            // });
-            //NextResponse.status(200).json({ fields, files })
-        })
-    })
+    try {
+        //const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        const filename = `${file.name.replace(
+            /\.[^/.]+$/,
+            ""
+        )}.${mime.getExtension(file.type)}`;
+        console.log(filename);
+        await writeFile(`${uploadDir}/${filename}`, buffer);
+        return NextResponse.json({ fileUrl: `${relativeUploadDir}/${filename}` });
+    } catch (e) {
+        console.error("Error while trying to upload a file\n", e);
+        return NextResponse.json(
+            { error: "Something went wrong." },
+            { status: 500 }
+        );
+    }
 }
