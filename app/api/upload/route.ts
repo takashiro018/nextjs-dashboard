5f352c2d72b593ed8f89290aff176a1843d08993
takashiro018 <shiro.takahashi@jade.finance>
2024-02-20 14:41:33 +1000

    commiting files

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,11 +1,8 @@
 import { handleUpload, type HandleUploadBody } from '@vercel/blob/client';
-import type { NextApiResponse, NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
 
-export async function handler(
-    request: NextApiRequest,
-    response: NextApiResponse,
-) {
-    const body = request.body as HandleUploadBody;
+export async function POST(request: Request): Promise<NextResponse> {
+    const body = (await request.json()) as HandleUploadBody;
 
     try {
         const jsonResponse = await handleUpload({
@@ -33,21 +30,22 @@ export async function handler(
                 // Use ngrok or similar to get the full upload flow
 
                 console.log('blob upload completed', blob, tokenPayload);
-                console.log(response);
+
                 try {
                     // Run any logic after the file upload completed
                     // const { userId } = JSON.parse(tokenPayload);
                     // await db.update({ avatar: blob.url, userId });
-
                 } catch (error) {
                     throw new Error('Could not update user');
                 }
             },
         });
 
-        return response.status(200).json(jsonResponse);
+        return NextResponse.json(jsonResponse);
     } catch (error) {
-        // The webhook will retry 5 times waiting for a 200
-        return response.status(400).json({ error: (error as Error).message });
+        return NextResponse.json(
+            { error: (error as Error).message },
+            { status: 400 }, // The webhook will retry 5 times waiting for a 200
+        );
     }
 }
