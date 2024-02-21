09bd668f10e8d13a8b55f0127b4cf515821c45db
takashiro018 <shiro.takahashi@jade.finance>
2024-02-21 11:36:22 +1000

    commiting files

--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -157,7 +157,7 @@ export default function Form() {
                                         <span>Upload a file</span>
                                         <input id="image_url" name="image_url" type="file" className="sr-only"
                                             onChange={uploadToClient}
-                                            ref={inputFileRef} />
+                                            ref={inputFileRef} value={blob?.url} />
                                     </label>
                                     <p className="pl-1">or drag and drop</p>
                                 </div>
